26f95e05a020e0d0a0308484c0b4050e3d92998c
Ronnie Martin <ronniemartin0913@gmail.com>
2022-08-25 14:52:12 +0800

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ import {
   FaFacebook,
 } from "react-icons/fa";
 
-import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
@@ -22,7 +21,7 @@ const Navbar = () => {
       <div>
         <img src={Logo} alt="Logo Image" style={{ width: "80px" }} />
       </div>
-      {/* menu */}
+
       <ul className='hidden md:flex'>
         <li>
           <Link to='home' smooth={true} duration={500}>
@@ -50,11 +49,11 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
-      {/* Hamburger */}
+
       <div onClick={handleClick} className="md:hidden z-10">
        {!nav ? <FaBars /> : <FaTimes />}
       </div>
-      {/* Mobile menu */}
+
       <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
       <li className='py-6 text-4xl'>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
@@ -62,31 +61,31 @@ const Navbar = () => {
           </Link>
         </li>
         <li className='py-6 text-4xl'>
-          {' '}
+  
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li className='py-6 text-4xl'>
-          {' '}
+  
           <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li className='py-6 text-4xl'>
-          {' '}
+
           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
             Work
           </Link>
         </li>
         <li className='py-6 text-4xl'>
-          {' '}
+
           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
-      {/* Social icons */}
+
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,6 @@ import Mongo from "../assets/mongo.png";
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-yellow-400 ">
@@ -27,19 +26,23 @@ const Skills = () => {
             <p className="my-4">HTML</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
             <p className="my-4">CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="Javascript icon"
+            />
             <p className="my-4">JAVASCRIPT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={ReactImg} alt="react icon" />
             <p className="my-4">REACT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={GitHub} alt="github icon" />
             <p className="my-4">GITHUB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
@@ -47,11 +50,11 @@ const Skills = () => {
             <p className="my-4">NODE JS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={Mongo} alt="mongo icon" />
             <p className="my-4">MONGO DB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-40 mx-auto" src={PHP} alt="HTML icon" />
+            <img className="w-40 mx-auto" src={PHP} alt="php icon" />
             <p className="my-4">PHP</p>
           </div>
         </div>
